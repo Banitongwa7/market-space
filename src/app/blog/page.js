4646071573edc393c6c3e3f01a1fd516825fbfc1
David BANITONGWA <davidbanitongwa@gmail.com>
2024-03-19 21:32:47 +0100

    update

---
+++ src/app/blog/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='text-3xl font-bold flex justify-center items-center h-screen'>
+        <div>This is blog page</div>
+    </div>
+  )
+}
